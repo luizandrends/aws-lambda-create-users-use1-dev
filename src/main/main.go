73a4b77d7fc231d5bb1d011df160c6b5168b1037
Luiz André Nunes da Silva <luiz.nunes@picpay.com>
2023-12-08 14:57:09 -0300

    feat: Criando workflow

---
+++ src/main/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func main() {
+	handler := http.HandlerFunc(handleRequest)
+	http.Handle("/users/create", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
+
+	resp := make(map([string]string))
+	resp["isBase64Encoded"] = false
+	resp["statusCode"] = 200
+	resp["statusDescription"] = "200 OK"
+	resp["body"] = "Hello World"
+
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+	}
+	w.Write(jsonResp)
+	return
+}
