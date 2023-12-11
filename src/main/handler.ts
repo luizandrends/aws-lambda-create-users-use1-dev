eeb801000732c51cf92c2b8a3dc76757bca241bd
Luiz André Nunes da Silva <luiz.nunes@picpay.com>
2023-12-11 17:20:25 -0300

    fix: moving dependencies to dist file

--- src/main/handler.ts
+++ src/main/handler.ts
@@ -1,5 +1,6 @@
 import { ALBEvent, ALBResult } from 'aws-lambda'
-import { PutItemCommand, DynamoDBClient } from '@aws-sdk/client-dynamodb'
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
+import { PutCommand, DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb'
 import { hash } from 'bcryptjs'
 import { v4 as uuid } from 'uuid'
 
@@ -10,28 +11,41 @@ type LoadBalancerRequestEventInterface = {
 }
 
 export const handleRequest = async (event: ALBEvent): Promise<ALBResult> => {
-  const dynamoDbService = new DynamoDBClient({ region: 'us-east-1' })
-
+  const client = new DynamoDBClient({ region: 'us-east-1' })
+  const docClient = DynamoDBDocumentClient.from(client)
   const requestBody: LoadBalancerRequestEventInterface = JSON.parse(
     event.body as string,
   )
 
-  const hashedPassword = await hash(requestBody.password, 8)
-  const params = {
-    TableName: 'aws-dynamodb-users-table-use1-dev',
-    Item: {
-      id: { S: uuid() },
-      name: { S: requestBody.name },
-      email: { S: requestBody.email },
-      password: { S: hashedPassword },
-    },
-  }
-  await dynamoDbService.send(new PutItemCommand(params))
+  try {
+    const hashedPassword = await hash(requestBody.password, 8)
 
-  return {
-    isBase64Encoded: false,
-    statusCode: 200,
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify("{'message': 'User created successfully'}"),
+    console.log(requestBody)
+
+    const command = new PutCommand({
+      TableName: 'aws-dynamodb-users-table-use1-dev',
+      Item: {
+        id: uuid(),
+        name: requestBody.name,
+        email: requestBody.email,
+        password: hashedPassword,
+      },
+    })
+
+    const response = await docClient.send(command)
+
+    return {
+      isBase64Encoded: false,
+      statusCode: 200,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(response),
+    }
+  } catch (err) {
+    return {
+      isBase64Encoded: false,
+      statusCode: 500,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify("{'message': 'Internal Server Error'}"),
+    }
   }
 }
