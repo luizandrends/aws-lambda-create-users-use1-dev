55ebdc6c42236f062141e626b3e0847747f936a6
Luiz André Nunes da Silva <luiz.nunes@picpay.com>
2023-12-11 18:37:15 -0300

    fix: created at and updated at

--- src/main/handler.ts
+++ src/main/handler.ts
@@ -17,37 +17,36 @@ export const handleRequest = async (event: ALBEvent): Promise<ALBResult> => {
   const requestBody: LoadBalancerRequestEventInterface = JSON.parse(
     event.body as string,
   )
-  const hashedPassword = await hash(requestBody.password, 8)
-
-  const command = new PutCommand({
-    TableName: 'aws-dynamodb-users-table-use1-dev',
-    Item: {
-      id: uuid(),
-      name: requestBody.name,
-      email: requestBody.email,
-      password: hashedPassword,
-      created_at: new Date().toISOString(),
-      updated_at: new Date().toISOString(),
-    },
-  })
-
-  const timeout = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      reject(new Error('Request timed out'))
-    }, 5000) // 5 seconds timeout
-  })
 
   try {
-    const response = await Promise.race([docClient.send(command), timeout])
-    console.log(response)
-  } catch (error) {
-    console.error('An error occurred:', error)
-  }
+    const hashedPassword = await hash(requestBody.password, 8)
 
-  return {
-    isBase64Encoded: false,
-    statusCode: 200,
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify('Done'),
+    const command = new PutCommand({
+      TableName: 'aws-dynamodb-users-table-use1-dev',
+      Item: {
+        id: uuid(),
+        name: requestBody.name,
+        email: requestBody.email,
+        password: hashedPassword,
+        created_at: new Date().toISOString(),
+        updated_at: new Date().toISOString(),
+      },
+    })
+
+    const response = await docClient.send(command)
+
+    return {
+      isBase64Encoded: false,
+      statusCode: 200,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(response),
+    }
+  } catch (err) {
+    return {
+      isBase64Encoded: false,
+      statusCode: 500,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify("{'message': 'Internal Server Error'}"),
+    }
   }
 }
