14ae2b9fc0f31c9c95951dd7b0ebdaae8b99271b
Luiz André Nunes da Silva <luiz.nunes@picpay.com>
2023-12-11 19:35:38 -0300

    fix: created at and updated at

--- src/main/handler.ts
+++ src/main/handler.ts
@@ -22,37 +22,37 @@ export const handleRequest = async (event: ALBEvent): Promise<ALBResult> => {
   const creationDate = new Date().toISOString()
   const toStringDate = creationDate.toString()
 
-  try {
-    const hashedPassword = await hash(requestBody.password, 8)
+  const hashedPassword = await hash(requestBody.password, 8)
 
-    const params = {
-      TableName: 'aws-dynamodb-users-table-use1-dev',
-      Item: {
-        id: { S: uuid() },
-        name: { S: requestBody.name },
-        email: { S: requestBody.email },
-        password: { S: hashedPassword },
-        created_at: { S: toStringDate },
-        updated_at: { S: toStringDate },
-      },
-    }
-
-    const response = ddb.putItem(params)
-
-    console.log(response)
-
-    return {
-      isBase64Encoded: false,
-      statusCode: 200,
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify('User Created'),
-    }
-  } catch (err) {
-    return {
-      isBase64Encoded: false,
-      statusCode: 500,
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify("{'message': 'Internal Server Error'}"),
-    }
+  const params = {
+    TableName: 'aws-dynamodb-users-table-use1-dev',
+    Item: {
+      id: { S: uuid() },
+      name: { S: requestBody.name },
+      email: { S: requestBody.email },
+      password: { S: hashedPassword },
+      created_at: { S: toStringDate },
+      updated_at: { S: toStringDate },
+    },
   }
+
+  const response = ddb.putItem(params, (err, data) => {
+    if (err) {
+      return {
+        isBase64Encoded: false,
+        statusCode: 500,
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify("{'message': 'Internal Server Error'}"),
+      }
+    } else {
+      return {
+        isBase64Encoded: false,
+        statusCode: 200,
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+      }
+    }
+  })
+
+  console.log(response)
 }
