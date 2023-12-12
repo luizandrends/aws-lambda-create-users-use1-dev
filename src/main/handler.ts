ef7bc44cd6e0f62485f9dec21889a4a159b7bf5c
Luiz André Nunes da Silva <luiz.nunes@picpay.com>
2023-12-12 00:54:02 -0300

    feat: creating email validation

--- src/main/handler.ts
+++ src/main/handler.ts
@@ -44,7 +44,7 @@ export const handleRequest = async (event: ALBEvent): Promise<ALBResult> => {
 
   const findUserByEmailParams = {
     TableName: tableName,
-    Key: { email: email },
+    Key: { email: { S: email } },
   }
 
   try {
